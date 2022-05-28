d1cc6b806d9b71b6d1333ecb8ef6e55ecd940bcb
Marcin Frydrych <marcinf23@gmail.com>
2022-05-28 14:48:58 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Marcin Frydrych on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
